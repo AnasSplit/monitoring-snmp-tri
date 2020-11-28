268ee21dc4cdcca3a593bc396fc4b3b1300a8693
AnasSplit <asimri62@gmail.com>
2020-11-28 13:22:05 +0100

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { MonitoringComponent } from './monitoring/monitoring.component';
+import { AboutComponent } from './about/about.component';
+
+const appRoutes: Routes = [
+  {path: 'about', component: AboutComponent},
+  { path: '', component: MonitoringComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    MonitoringComponent
+    MonitoringComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
