231792ba6c6e79c06059b6141f90c47c53866f23
Anas Simri <asim@patek.com>
2020-12-04 19:38:09 +0100

    Commit methodes

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { UserRepository } from '../repository/user';
 import axios from 'axios'
+import { ThisReceiver } from '@angular/compiler';
 
 interface Hello {
   hello: any;
@@ -15,17 +16,35 @@ interface Hello {
 })
 
 export class ServicesComponent {
+  //Variables globales :
+
+  //Attributs
   Name: '';
   IP = '';
   Marque = '';
   VersionSNMP: '';
   Credentials: '';
+
+  //Métriques à monitorer
   Int: '';
   CPU: '';
-  reponsesnmp: '';
+  True = 'True';
+  False = 'False';
 
+  //Interface avec la bdd
+  Basedd: '';
+  reponsesnmpcpu: '';
+  reponsesnmpint: '';
+
+  //OIDs
+  OIDint = '1.3.6.1.2.1.2.2.1.5.1';
+  OIDcpu = '1.3.6.1.4.1.9.9.109.1.1.1.1.25';
+
+  //Hosts
   hostForm: FormGroup;
   hostForm2: FormGroup;
+
+  //TestHelloAPIRest
   hello: Hello | null = null;
 
   constructor(private fb: FormBuilder, private userRepo: UserRepository, private httpClient: HttpClient) {
@@ -37,6 +56,7 @@ export class ServicesComponent {
 
   ngOnInit() {
     this.syncAPI();
+    this.getMachine();
   }
 
   async clearAPI() {
@@ -56,8 +76,51 @@ export class ServicesComponent {
     return this.hostForm.get("Hosts") as FormArray
   }
 
-  postMachine(){
-    axios.post('https://snmp-server.herokuapp.com//addclient/'+this.Name+'/'+this.IP+'/'+this.Marque)
+  postMachine() {
+    axios.post('https://snmp-server.herokuapp.com/addclient/' + this.Name + '/' + this.IP + '/' + this.Marque);
+  }
+
+  deleteMachine() {
+    axios.delete('https://snmp-server.herokuapp.com/deleteclient/' + this.Name + '/' + this.IP + '/' + this.Marque);
+  }
+
+  getMachine() {
+    axios.get('https://snmp-server.herokuapp.com/showclients')
+      .then(function (response) {
+        this.infos = response;
+        console.log(this.infos);
+        this.Basedd = response;
+      })
+  }
+
+  SNMPreq() {
+    if (this.CPU === this.True) {
+      this.CheckCPU();
+      console.log('Check CPU executed.');
+    }
+
+    if (this.Int === this.True) {
+      this.CheckInt();
+      console.log('Check Int executed.');
+    }
+  }
+
+  CheckCPU() {
+    axios.get('http://snmp-server.herokuapp.com/informations/' + this.OIDcpu + '/' + this.IP + '/' + this.Credentials)
+      .then(function (response) {
+        this.infos = response
+        console.log(this.infos)
+        this.reponsesnmpcpu = response;
+      })
+  }
+
+  CheckInt() {
+    axios.get('https://snmp-server.herokuapp.com/showclients' + this.OIDint + '/' + this.IP + '/' + this.Credentials)
+      .then(function (response) {
+        this.infos = response;
+        console.log(this.infos);
+        this.reponsesnmpint = response;
+      })
   }
 
   newHost(): FormGroup {
@@ -77,7 +140,7 @@ export class ServicesComponent {
     console.log(this.Name);
     console.log(this.IP);
     console.log(this.Marque);
-    //this.postMachine();
+    this.postMachine();
   }
 
   addHost() {
@@ -86,6 +149,7 @@ export class ServicesComponent {
 
   removeIP(i: number) {
     this.Hosts().removeAt(i);
+    this.deleteMachine();
   }
 
   onSubmit() {
